2f3bbd6f5a1a2d7734055fbd12d4232f64958f45
Andrew Nelder <andrewnelder@gmail.com>
2013-07-30 15:58:20 -1000

    fixed header dict and text_body

--- alphamail/handler.py
+++ alphamail/handler.py
@@ -17,49 +17,51 @@ class MailBox(object):
         server = ImapTransport(hostname, ssl=ssl)
         self.connection = server.connect(username, password)
 
-    def parse_email(self, raw_email):
+    def parse_email(self, raw_email, uid):
 
         email_message = email.message_from_string(raw_email)
 
         maintype = email_message.get_content_maintype()
 
         text_body = []
-
         if maintype == 'multipart':
             for part in email_message.walk():
                 if part.get_content_type() == "text/plain":
                     text_body.append(part.get_payload(decode=True))
         elif maintype == 'text':
             text_body.append(email_message.get_payload(decode=True))
+        text_body = " ".join(text_body)
 
         email_dict = dict(email_message.items())
-
         from_dict = {}
         from_ = email.utils.parseaddr(email_dict['From'])
         if len(from_) == 2:
-            from_dict = {'Name': from_[0], 'Email': from_[1]}
+            from_dict = {'name': from_[0], 'email': from_[1]}
 
         to_dict = {}
         to_ = email.utils.parseaddr(email_dict['To'])
         if len(to_) == 2:
-            to_dict = {'Name': to_[0], 'Email': to_[1]}
+            to_dict = {'name': to_[0], 'email': to_[1]}
 
         subject = email_dict.get('Subject', None)
+        if subject:
+            subject = subject.replace("\n", "").replace("\r", "")
+
         date = email_dict.get('Date', None)
         message_id = email_dict.get('Message-ID', None)
 
         # Get the headers
-        headers = []
+        headers = {}
         headers_keys = ['Received-SPF', 'MIME-Version', 'X-Spam-Status', 'X-Spam-Score']
 
         for key in headers_keys:
             header_value = email_dict.get(key)
-
             if header_value:
-                headers.append({'Name': key, 'Value': header_value})
+                headers[key] = header_value
 
         return {
-            'id': message_id,
+            'id': uid,
+            'message-id': message_id,
             'from_address': from_dict,
             'to_address': to_dict,
             'subject': subject,
@@ -71,7 +73,7 @@ class MailBox(object):
     def fetch_message(self, uid):
         _, data = self.connection.uid('fetch', uid, '(RFC822)')
         raw_email = data[0][1]
-        email_metadata = self.parse_email(raw_email)
+        email_metadata = self.parse_email(raw_email, uid)
         return email_metadata
 
     def fetch_messages(self, uids):
